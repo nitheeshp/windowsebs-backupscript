613ad271bc472833073440780756ce3b290206ad
nitheeshp <nitheeshp@outlook.com>
2014-02-08 12:55:15 +0530

    Update windows_ec2_snapshot .bat

--- windows_ec2_snapshot .bat
+++ windows_ec2_snapshot .bat
@@ -8,5 +8,5 @@ CMD /C ec2-create-snapshot %AWS_VOLUME% -d "Daily Snapshot"
 :: Find old snapshots for this volume, sort them by date desc
 ec2-describe-snapshots -F "volume-id=%AWS_VOLUME%" -F "status=completed"|find /I "Daily Snapshot"|sort /R /+49>%AWS_HOME%\snapshots.txt
 
-:: Loop over old snapshots, skip the first 10, delete the rest
+:: Loop over old snapshots, skip the first 7, delete the rest
 for /f "tokens=2 skip=%AWS_SNAPSHOT_KEEP%" %%s in (%AWS_HOME%\snapshots.txt) do ec2-delete-snapshot %%s
